84e41d47da8aaa423f40dff83a620e7e03bf051b
Hugo-AP <55633259+Hugo-AP@users.noreply.github.com>
2020-11-12 19:43:13 -0500

    first_commit_to_master

---
+++ src/main/java/com/hacknet/wheelsy/WheelsyApplication.java
@@ -0,0 +1,13 @@
+package com.hacknet.wheelsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheelsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheelsyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hacknet/wheelsy/WheelsyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacknet.wheelsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheelsyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
